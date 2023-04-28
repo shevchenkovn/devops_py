ec8c22db2688030bff576c081c7aa66fa432b169
shevchenkovn <shevchenkoveronika19@gmail.com>
2023-04-28 18:48:03 +0300

    fix my hw2, removed unnecessary file copy

--- do_hw2.py
+++ do_hw2.py
@@ -1,9 +1,22 @@
 import requests
+result =[] ## !=200 response status code urls
+urls = [
+    "https://google.com",
+    "https://www.mcdonalds.com/ru/",
+    "https://www.kfc-ukraine.com/ru/"
+    ]
 
-url = "https://google.com"
-if url.__contains__('http') and url.__contains__('://'):
-    print("url looks valid")
-    responce = requests.get(url, timeout = 10)
-    print("getRequest status code = {}\nTimeout = {}".format(responce.status_code, 10))
-else:
-    print("check input url and try again")
+for url in urls:
+    if url.__contains__('http') and url.__contains__('://'):
+        print("url looks valid")
+        responce = requests.get(url, timeout = 10)
+        if responce.status_code != 200:
+            result.append(url)
+
+        print("getRequest status code = {}\nTimeout = {}".format(responce.status_code, 10))
+    else:
+        print("check input url and try again")
+
+
+print("Urls that didnt gave 200 status code:")
+print(result)
